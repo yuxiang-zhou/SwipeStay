e84efcbcb56c9c6e43269155cffc4fc638b6344b
Yuxiang Zhou <mr.yuxiang.zhou@googlemail.com>
2017-07-02 21:17:02 +0100

    provided hotel data

---
+++ frontend/src/providers/hotel-data.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+import { Http } from '@angular/http';
+
+import { UserData } from './user-data';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
+
+
+@Injectable()
+export class ConferenceData {
+
+  host: string = 'http://178.62.38.12/api';
+
+  constructor(public http: Http, public user: UserData) { }
+
+  load(condition: any): any {
+    return this.http.get(this.host + '/hotel/filter/1').map(this.processData, this);
+  }
+
+  loadNearest(location: any) {
+    return this.load({});
+  }
+
+  processData(data: any) {
+    // just some good 'ol JS fun with objects and arrays
+    // build up the data by linking speakers to sessions
+    let hotels = data.json();
+    hotels.forEach((hotel: any) => {
+      // post process datas
+      hotel.rating = parseInt(hotel.rating);
+      hotel.price = parseInt(hotel.price);
+
+    });
+    return hotels;
+  }
+
+}
